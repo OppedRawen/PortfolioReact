78e765cacbc8b401aca1ccf81eb304acd631dc4e
OppedRawen <yud2372@gmail.com>
2023-05-06 23:26:56 -0700

    added base template and tailwind setup

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    
+    <main className="">
+      
+
+    {/* section */}
+    <section className=' bg-blue-700 min-h-screen'>
+      <h1>Hello</h1>
+    </section>
+
+     
+    </main>
+  )
+}
